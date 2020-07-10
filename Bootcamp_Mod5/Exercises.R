cec63330ceb1d50b076e4dc8145a859dd71c2105
kipmccharen <kip.mccharen@gmail.com>
2020-07-10 13:59:37 -0400

    finished the hard parts, now cleaning up

--- Bootcamp_Mod5/Exercises.R
+++ Bootcamp_Mod5/Exercises.R
@@ -5,7 +5,7 @@ checkrealanimal <- function() {
   flnm <- "animals.txt"
   success <- FALSE
   while (!success) {
-    testanimal <- readline(prompt="What is your favorite animal (singular form)?")
+    testanimal <- readline(prompt="What is your favorite animal (singular form)?   ->  ")
     my_data <- tolower(readChar(flnm, file.info(flnm)$size))
     my_data <- str_replace_all(my_data, "[\n]", " ")
     
@@ -14,7 +14,11 @@ checkrealanimal <- function() {
       print("not quite, try again")
     }
   }
-  return("that's a real animal, nice one!")
+  print('Hmm...   ')
+  for (let in strsplit(testanimal, '')) {
+    print(let)
+  }
+  return("Yeah that sounds right! That's a real animal, nice one!")
 }
 
 print(paste('current time is: ', format(Sys.time(), "%a %b %d %X %Y")))
--- COVID_visualizing.R
+++ COVID_visualizing.R
@@ -5,40 +5,70 @@ library(lubridate)
 library(Hmisc)
 library(ggplot2)
 library(scales)
+##Graph #1 limited country log graph
+
+rdata <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+rdata[[1]] <- as.Date(rdata[[1]], "%d/%m/%Y")
 ##
-data <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
-data[[1]] <- as.Date(data[[1]], "%d/%m/%Y")
+rdata <- select(rdata, dateRep, countriesAndTerritories, cases, popData2019)
+rdata <- rdata %>% drop_na(dateRep)
+rdata <- rdata %>% arrange(countriesAndTerritories,dateRep)
 ##
-data <- select(data, dateRep, countriesAndTerritories, cases, popData2019)
-data <- data %>% drop_na(dateRep)
-data <- data %>% filter(dateRep >= "2020-01-22" & dateRep <= "2020-07-01")
+rdata <- rdata %>% group_by(countriesAndTerritories) %>% mutate(cum_cases = cumsum(cases))
+head(rdata %>% filter(countriesAndTerritories == "China"))
+rdata <- ungroup(rdata)
+rdata[2] <- lapply(rdata[2], as.character)
+rdata[4] <- lapply(rdata[4], as.numeric)
+rdata$casesPerMillion <- with(rdata, cum_cases / (popData2019 / 1000000))
 ##
-data <- data %>% arrange(countriesAndTerritories,dateRep)
-data <- data %>% group_by(countriesAndTerritories) %>% mutate(cum_cases = cumsum(cases))
-head(data %>% filter(countriesAndTerritories == "China"))
-##
-datag <- data %>% group_by(dateRep)
+datag <- rdata %>% group_by(dateRep)
 datag <- datag %>% summarise(cases = sum(cases), cum_cases = sum(cum_cases))
 datag$popData2019 <- 7669215953
 datag$countriesAndTerritories <- 'World'
+datag$casesPerMillion <- with(datag, cum_cases / (popData2019 / 1000000))
 datag <- datag[names(data)]
-describe(datag)
-# describe(data)
-#
-dtogether <- bind_rows(datag, data)
-head(dtogether)
 ##
-dtogether$casesPerMillion <- with(dtogether, cum_cases / (popData2019 / 1000000))
+dtogether <- bind_rows(datag, rdata)
+write.csv(dtogether,"D:\\Git\\COVID-Analysis\\dtogether.csv", col.names = TRUE)
+##
+dtogether <- dtogether %>% filter(dateRep >= "2020-01-22" & dateRep <= "2020-07-06")
+##
 countrylist <- c('United_States_of_America', 'United_Kingdom', 'South_Korea', 'China', 'World')
 dtogether <- dtogether %>% filter(countriesAndTerritories %in% countrylist)
 head(dtogether)
 ##
-
 theme_set(theme_minimal())
 ggplot(data=dtogether, aes(x=dateRep, y=casesPerMillion)) +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   geom_line(aes(color = countriesAndTerritories)) #, linetype = countriesAndTerritories))
+##
 
+################################
+################################
+##Graph #2 world heatmap
 
+library(ggplot2)
+library(maps)
+library(ggthemes)
+library(sf)
+library(spData)
 
+lastday = max(rdata$dateRep)
+rdata <- rdata %>% filter(dateRep == lastday)
+head(rdata)
+
+# Exported datasets and added iso_a2 to associate countries with dataset
+# write.csv(rdata,"D:\\Git\\COVID-Analysis\\rdata.csv")
+# write.csv(world,"D:\\Git\\COVID-Analysis\\world.csv", col.names = TRUE)
+
+country_data <- read.csv("D:\\Git\\COVID-Analysis\\rdata.csv")
+total <- merge(country_data,world,by=c("iso_a2"))
+
+worldmap <- ggplot(data = total) +
+  theme_map() +
+  coord_cartesian(ylim = c(-50, 90)) + 
+  geom_sf(aes(fill = casesPerMillion, geometry = geom)) +
+  scale_fill_viridis_c(option = "plasma", trans = "sqrt")
+  
+worldmap
